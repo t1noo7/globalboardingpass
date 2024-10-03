5eb993db79c0fce7de09bd3f14051a3543a1c58d
†inØ <2121030019@student.humg.edu.vn>
2024-10-04 00:09:23 +0700

    Update cloud_types.rb

--- globalboardingpass/cloud_types.rb
+++ globalboardingpass/cloud_types.rb
@@ -1,9 +1,21 @@
 module CloudTypes
   CLOUDLABELS = [
-    "Username",
+    "quarantine",
+    "food",
+    "company",
+    "podcast",
+    "2021",
+    "travel",
+    "username"
   ]
 
   CLOUDPROMPTS = [
+    "Favourite Quarantine Passtime? :lock: :tennis: :video_game:",
+    "Favourite Food? :yum: :pizza: :cookie:",
+    "Favourite/Dream Company? :money_with_wings: :star2: :chart_with_upwards_trend:",
+    "What podcast are you currently listening to? :microphone: :headphones: :notes:",
+    "What are you looking forward to in 2021? :tada: :sunglasses: :confetti_ball:",
+    "Where are you hoping to travel next? :airplane: :earth_americas: :luggage:"
     "Global Boarding Pass 🖍️🫠"
   ]
 end
